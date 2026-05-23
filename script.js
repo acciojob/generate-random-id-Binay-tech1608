0442971dbe3c1cef7aeafab0918e83734299b3ce
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-05-23 12:23:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ function makeid(l) {
 	const char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 	for(let i=0; i<l; i++){
-		res += char_list.charAt(
-			Math.floor(
-				Math.random()*(char_list.length);
-			)
-		);
+		res += char_list.charAt(Math.floor(Math.random()*(char_list.length)));
 	}
 	return res;
 }
 
 // Do not change the code below.
 
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
